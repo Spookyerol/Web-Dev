2e52181499a7421eb69372a54dec38b5c713431c
Spookyerol <erdalinko@gmail.com>
2019-01-27 20:43:10 +0000

    Merge local with master

---
+++ app.js
@@ -0,0 +1,340 @@
+// JavaScript source code
+let users = [
+    {
+        "username": "doctorwhocomposer",
+        "password": "password",
+        "forename": "Delia",
+        "surname": "Derbyshire",
+        "email": "Derby@mail.com",
+        "status": "Status: Feeling who?",
+        "access_token": "concertina"
+    },
+
+    {
+        "username": "Joeboy",
+        "password": "pass",
+        "forename": "Noname",
+        "surname": "Johnson",
+        "email": "johnny@mail.com",
+        "status": "Status: I am the one with no name.",
+        "access_token": ""
+    }
+
+]
+
+let comments = [];
+
+ex_user = { //Example object
+    "username": "doctorwhocomposer",
+    "password": "password",
+    "forename": "Delia",
+    "surname": "Derbyshire",
+    "email": "Derby@mail.com",
+    "status": "Status: Feeling who?",
+    "access_token": "concertina"
+};
+
+ex_comment = { //Example comment
+    "username": "commenter",
+    "content": "SampleText"
+}
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('client-sessions');
+const ejs = require('ejs');
+const handlebars = require('handlebars');
+
+function findUser(username) {
+    for (let i in users) {
+        if (users[i].username === username) return users[i];
+    }
+}
+
+function filterComments(username) {
+    filtered = [];
+    for (let i in comments) {
+        if (comments[i].username === username) {
+            filtered.push(comments[i]);
+        }
+    }
+    return filtered;
+}
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static(__dirname + '/views'));
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: './views' });
+});
+
+app.use(session( {
+    cookieName: 'userSession',
+    duration: 24 * 60 * 60 * 1000,
+    activeDuration: 60 * 60 * 1000,
+    secret: 'sekrit',
+    cookie: {
+        ephemeral: true,
+        httpOnly: true,
+        secure: false
+    }
+}));
+
+app.post('/login', function (req, res) {
+    let message;
+    let status;
+    for (let i in users) {
+        if (users[i].username != req.body.username) {
+            message = "Username does not exist.";
+            status = 403;
+        }
+        else {
+            if (users[i].password != req.body.password) {
+                message = "Incorrect password.";
+                status = 403;
+                break;
+            }
+            else {
+                req.userSession.loggedIn = true;
+                req.userSession.username = req.body.username;
+                req.userSession.access_token = findUser(req.body.username).access_token;
+                message = "Login Successful";
+                status = 200;
+                break;
+            }
+        }
+    }
+    res.status(status);
+    res.send(message);
+});
+
+app.get('/logout', function (req, res) {
+    req.userSession.reset();
+    res.redirect('/');
+});
+
+app.post('/register', function (req, res, next) {
+    const person = req.body.username;
+    for (let i in users) {
+        if (users[i].username === person) {
+            console.log("Registration request for " + person + " failed. Cause: Username taken.");
+            res.status(400);
+            res.send("Username taken, please try again.");
+            return 0;
+        }
+    }
+    if (req.body.password != req.body.retypePass) {
+        console.log("Registration request for " + person + " failed. Cause: Passwords do not match.");
+        res.status(400);
+        res.send("Passwords do not match, please try again.");
+        return 0;
+    }
+    users.push({
+        "username": person,
+        "password": req.body.password,
+        "forename": req.body.forename,
+        "surname": req.body.surname,
+        "email": req.body.email,
+        "status": "Status: Feeling blue.",
+        "access_token": ""
+    });
+
+    req.userSession.loggedIn = true;
+    req.userSession.username = person;
+    req.userSession.access_token = "";
+    console.log("Registration request for user " + person + " successful.");
+    res.status(200);
+    res.send("Registration successful");
+});
+
+app.post('/people', function (req, res, next) {
+    const person = req.body.username;
+    if (req.body.access_token != "concertina") {
+        res.status(403);
+        res.send("403 Unautharized access_token.");
+        return 0;
+    }
+    for (let i in users) {
+        if (users[i].username === person) {
+            console.log("Registration request for " + person + " failed. Cause: Username taken.");
+            res.status(400);
+            res.send("Username taken, please try again.");
+            return 0;
+        }
+    }
+    if (req.body.password != req.body.retypePass) {
+        console.log("Registration request for " + person + " failed. Cause: Passwords do not match.");
+        res.status(400);
+        res.send("Passwords do not match, please try again.");
+        return 0;
+    }
+    users.push({
+        "username": person,
+        "password": req.body.password,
+        "forename": req.body.forename,
+        "surname": req.body.surname,
+        "email": req.body.email,
+        "status": "Status: Feeling blue.",
+        "access_token": ""
+    });
+
+    req.userSession.loggedIn = true;
+    req.userSession.username = person;
+    req.userSession.access_token = "";
+    console.log("Registration request for user " + person + " successful.");
+    res.status(200);
+    res.send("Registration successful");
+});
+
+app.get('/people', function (req, res) {
+    res.status(200);
+    res.json({ users: users });
+});
+
+app.get('/people/doctorwhocomposer', function (req, res) {
+    res.status(200);
+    const delia = findUser("doctorwhocomposer");
+    res.json(delia);
+});
+
+app.use(function (req, res, next) {
+    if (req.userSession.loggedIn) {
+        next();
+    }
+    else {
+        res.status(403).json({
+            message: "403 Request Denied.",
+        })
+    }
+});
+
+app.get('/main', function (req, res) {
+    const user = findUser(req.userSession.username);
+    status = user.status;
+    if (status) {
+        res.status(200);
+        res.json({
+            "username": user,
+            "status": status,
+            "comments": comments
+        });
+    }
+    else {
+        res.status(400);
+        res.send("A problem has occured.");
+    }
+});
+
+app.post('/main/comment', function (req, res) {
+    content = req.body.comment;
+    const user = req.userSession.username;
+    comment = { "username": user, "content": content }
+    comments.push(comment);
+    res.json(comments);
+});
+
+app.get('/main/comment', function (req, res) {
+    res.json(comments);
+});
+
+app.post('/main/status', function (req, res) {
+    const status = req.body.status;
+    const user = findUser(req.userSession.username);
+    user.status = statusl
+    res.status(200);
+    res.json(status);
+});
+
+app.get('/profile', function (req, res) {
+    const user = findUser(req.userSession.username);
+    res.status(200);
+    res.json({
+        "username": user.username,
+        "email": user.email,
+        "forename": user.forename,
+        "surname": user.surname,
+    });
+});
+
+app.post('/profile/password', function (req, res) {
+    const user = findUser(req.userSession.username);
+    if (req.body.oldPassword != user.password) {
+        res.status(401)
+        res.send("Your old password is incorrect.");
+        return 0;
+    }
+    else {
+        if (req.body.newPassword != req.body.newPassreType) {
+            res.status(401)
+            res.send("Your new password does not match.");
+            return 0;
+        }
+    }
+    user.password = req.body.newPassword;
+    res.status(200)
+    console.log("sent");
+    res.send("Password successfully changed.");
+});
+
+app.post('/profile/details', function (req, res) {
+    const user = finduser(req.userSession.username);
+    if (req.body.password != user.password) {
+        res.status(401)
+        res.send("Your password is incorrect.");
+        return 0;
+    }
+    else {
+        if (req.body.email != "") { //Block makes sure that fields left empty do not overwrite existing data
+            user.email = req.body.email;
+        }
+        if (req.body.forename != "") {
+            user.forename = req.body.forename;
+        }
+        if (req.body.surname != "") {
+            user.surname = req.body.surname;
+        }
+    }
+    res.status(200);
+    res.json({
+        "username": req.userSession.username,
+        "email": user.email,
+        "forename": user.forename,
+        "surname": user.surname
+    });
+});
+
+app.get('/friend', function (req, res) {
+    res.render('friends.html');
+});
+
+app.get('/people/user', function (req, res) {
+    let user = findUser(req.query.username);
+    if (user) {
+        res.status(200);
+        res.json({
+            "username": user.username,
+            "email": user.email,
+            "forename": user.forename,
+            "surname": user.surname,
+            "userStatus": user.status,
+            "comments": filterComments(user.username)
+        })
+    }
+    else {
+        res.status(404);
+        res.send("User does not exist.");
+    }
+});
+
+module.exports = app.listen(3000);
+//app.listen(3000, function () {
+//    console.log("Live at Port 3000");
+//});
---
+++ views/index.js
@@ -0,0 +1,329 @@
+// JavaScript source code for front-end client
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function makeBurger() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
+
+function navigateTo(pageName) {
+    $.ajax({
+        url: "/" + pageName,
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            let context = res;
+            $("a").removeClass("active");
+            if (pageName === 'profile') {
+                let source = $("#userData").html().trim();
+                let template = Handlebars.compile(source);
+                $("#profile").addClass("active");
+                $("#registerPage").css("display", "none");
+                $("#mainPage").css("display", "none");
+                $("#userPage").css("display", "none");
+                $("#profilePage").css("display", "block");
+                let html = template(context);
+                $("#userDetails").html(html);
+            }
+            else {
+                if (pageName === "main") {
+                    let source = $("#userStatus").html().trim();
+                    let template = Handlebars.compile(source);
+                    $("#main").addClass("active");
+                    $("#registerPage").css("display", "none");
+                    $("#profilePage").css("display", "none");
+                    $("#userPage").css("display", "none");
+                    $("#mainPage").css("display", "block");
+                    let html = template(context);
+                    $("#status").html(html);
+                    renderComments(res.comments, "#comments");
+                }
+                else {
+                    if (pageName === "people/user") {
+                        let source = $("#otherUserStatus").html().trim();
+                        let template = Handlebars.compile(source);
+                        $("#registerPage").css("display", "none");
+                        $("#profilePage").css("display", "none");
+                        $("#mainPage").css("display", "none");
+                        $("#userPage").css("display", "block");
+                        let html = template(context);
+                        $("#otherStatus").html(html);
+                        renderComments(res.comments, "#userComments");
+                        source = $("#userData").html().trim();
+                        template = Handlebars.compile(source);
+                        html = template(res);
+                        $("#otherUserDetails").html(html);
+                    }
+                }
+            }
+            console.log("Navigated to " + pageName);
+        },
+
+        error: function (res) {
+            alert("403 request denied.");
+            console.log("Failed to navigate to " + pageName);
+        },
+    });
+}
+
+function loginHandler() {
+    username = $("#username").val();
+    userdata = { "username": username, "password": $("#password").val() };
+    $.ajax({
+        url: "/login",
+        type: "POST",
+        data: JSON.stringify(userdata),
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            navigateTo('main');
+            console.log('Login success');
+        },
+
+        error: function (res) {
+            alert(res.responseText);
+            console.log('Login error');
+        }
+    });
+}
+
+function logoutHandler() {
+    $.ajax({
+        url: "/logout",
+        type: "GET",
+        success: function (res) {
+            console.log("Logged out.");
+        },
+
+        error: function (res) {
+            console.log("Failed to log out.");
+        }
+    });
+}
+
+function registerHandler() {
+    accountData = {
+        "username": $("#new_user").val(),
+        "password": $("#new_pass").val(),
+        "retypePass": $("#new_passRetype").val(),
+        "email": $("#new_mail").val(),
+        "forename": $("#forename").val(),
+        "surname": $("#surname").val()
+    };
+    $.ajax({
+        url: "/register",
+        type: "POST",
+        data: JSON.stringify(accountData),
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            navigateTo('main');
+            console.log('Registration success');
+        },
+
+        error: function (res) {
+            console.log(res.responseText);
+            console.log('Registration error');
+            alert(JSON.stringify(res.responseText));
+        },
+    });
+}
+
+function newComment() {
+    content = { "comment": $("#comment").val() };
+    $.ajax({
+        url: "/main/comment",
+        type: "POST",
+        data: JSON.stringify(content),
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            console.log(res);
+            renderComments(res, "#comments");
+            console.log("Comment added.");
+        },
+
+        error: function () {
+
+        }
+    });
+}
+
+function createComment(context) {
+    let source = $("#commentTemplate").html().trim();
+    let template = Handlebars.compile(source);
+    let html = template(context);
+    $("#comments").prepend(html);
+    return 0;
+}
+
+function renderComments(comments, target) {
+    $(target).empty();
+    for (let i in comments) {
+        let context = {
+            "username": comments[i].username,
+            "content": comments[i].content
+        };
+        let source = $("#commentTemplate").html().trim();
+        let template = Handlebars.compile(source);
+        let html = template(context);
+        $(target).prepend(html);
+    }
+    return 0;
+}
+
+function getComments() {
+    $.ajax({
+        url: "/main/comment",
+        type: "GET",
+        success: function (res) {
+            renderComments(res, "#comments");
+        },
+
+        error: function () {
+            console.log("Response not received.");
+        }
+    });
+}
+
+function changeStatus() {
+    $.ajax({
+        url: "/main/status",
+        type: "POST",
+        data: JSON.stringify({ "status": $("#status").text() }),
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            alert("Status updated.");
+            console.log("Status updated to: " + JSON.stringify(res.responseText));
+        },
+
+        error: function (res) {
+            alert("Status not updated. Please try again.");
+            console.log("Status not updated.");
+        }
+    });
+}
+
+function changePassword() {
+    data = {
+        "oldPassword": $("#oldPassword").val(),
+        "newPassword": $("#newPassword").val(),
+        "newPassreType": $("#newPassreType").val(),
+    }
+    $.ajax({
+        url: "/profile/password",
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            alert("Password changed successfully.");
+            console.log("Password changed successfully.");
+        },
+
+        error: function (res) {
+            alert(res.responseText);
+            console.log(res.responseText);
+        }
+    });
+}
+
+function changeDetails() {
+    data = {
+        "password": $("#pass").val(),
+        "email": $("#email").val(),
+        "forename": $("#firstName").val(),
+        "surname": $("#LastName").val()
+    }
+    $.ajax({
+        url: "/profile/details",
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            navigateTo('profile');
+            alert("Details successfully changed.");
+            console.log("Details changed successfully.");
+        },
+
+        error: function (res) {
+            alert(res.responseText);
+            console.log(res.responseText);
+        }
+    })
+}
+
+function searchUser() {
+    query = { "username": $("#userquery").val() };
+    $.ajax({
+        url: "/people/user",
+        type: "GET",
+        data: query,
+        contentType: "application/json; charset=utf-8",
+        success: function (res) {
+            let source = $("#otherUserStatus").html().trim();
+            let template = Handlebars.compile(source);
+            $("#registerPage").css("display", "none");
+            $("#profilePage").css("display", "none");
+            $("#mainPage").css("display", "none");
+            $("#userPage").css("display", "block");
+            let html = template(res);
+            $("#otherStatus").html(html);
+            renderComments(res.comments, "#userComments");
+            source = $("#userData").html().trim();
+            template = Handlebars.compile(source);
+            html = template(res);
+            $("#otherUserDetails").html(html);
+            console.log("User found.");
+            console.log(res);
+        },
+
+        error: function (res) {
+            alert(res.responseText);
+            console.log(res.responseText);
+        }
+    })
+}
+
+    /*let row = document.createElement("div");
+    row.classList ? row.classList.add('row') : col_3.className += ' row';
+    let col_3 = document.createElement("div");
+    col_3.classList ? col_3.classList.add('col-sm-3') : col_3.className += ' col-sm-3';
+    row.appendChild(col_3);
+    let well_3 = document.createElement("div");
+    well_3.classList ? well_3.classList.add('well', 'comment') : well_3.className += ' well' + 'comment';
+    col_3.appendChild(well_3);
+    let commenter = document.createElement("p");
+    commenter.appendChild(document.createTextNode(user));
+    well_3.appendChild(commenter);
+    let col_9 = document.createElement("div");
+    col_9.classList ? col_9.classList.add('col-sm-9') : col_9.className += ' col-sm-9';
+    row.appendChild(col_9);
+    let well_9 = document.createElement("div");
+    well_9.classList ? well_9.classList.add('well', 'comment') : well_9.className += ' well' + 'comment';
+    col_9.appendChild(well_9);
+    let comContent = document.createElement("p");
+    comContent.appendChild(document.createTextNode(content))
+    well_9.appendChild(comContent);*/
+
+//document.createComment(content)
+
+/*<div class="row">
+
+      <div class="col-sm-3">
+
+          <div class="well comment">
+              <p>John</p>
+          </div>
+
+      </div>
+
+      <div class="col-sm-9">
+
+          <div class="well comment">
+              <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+          </div>
+
+      </div>
+
+  </div>*/
